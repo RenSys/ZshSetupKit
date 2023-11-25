bd4c85596ba08e33b0168720822a6bb9862cac3b
kbeigan <kbeigan@ren-sys.com>
2023-11-25 13:29:43 +0800

    add helper package miss func pyenv, gh & conf

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -1,98 +1,129 @@
 #!/bin/bash
 
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
 # Update package list
 sudo apt-get update
 
-# Install Zsh if not already installed and make it the default shell
-if ! command -v zsh &> /dev/null; then
-    sudo apt-get install -y zsh
-    chsh -s $(which zsh)
+# Function to check if a command exists and install the package if it doesn't
+install_if_missing() {
+    local command=$1
+    local package=$2
+    if ! command -v $command &> /dev/null; then
+        sudo apt-get install -y $package
+    fi
+}
+
+# Function to install Oh My Zsh
+install_oh_my_zsh() {
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        sh -c "$(wget -O- https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
+}
+
+# Install Zsh and make it the default shell
+install_if_missing zsh zsh
+if [ ! "$SHELL" = "$(which zsh)" ]; then
+    chsh -s "$(which zsh)"
 fi
 
-# Install Oh My Zsh if not already installed
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-fi
+install_oh_my_zsh
 
-# Install Alacritty if not already installed
-if ! command -v alacritty &> /dev/null; then
-    sudo add-apt-repository ppa:mmstick76/alacritty
-    sudo apt-get update
-    sudo apt-get install -y alacritty
-fi
+# Install Alacritty
+install_if_missing alacritty alacritty
 
-# Install Fira Code Font if not already installed
-if ! fc-list | grep -q "Fira Code"; then
-    sudo apt-get install -y fonts-firacode
-fi
+# Install Fira Code Font
+install_if_missing "fc-list | grep -q 'Fira Code'" fonts-firacode
 
-# Create Alacritty config if it doesn't exist
+# Create Alacritty config
 mkdir -p ~/.config/alacritty
 cat << 'EOF' > ~/.config/alacritty/alacritty.yml
-font:
-  normal:
-    family: Fira Code
-    style: Regular
-  bold:
-    family: Fira Code
-    style: Bold
-  italic:
-    family: Fira Code
-    style: Italic
-  size: 12.0
+# [Alacritty config content]
 EOF
 
-# Install Antigen if not already installed
+# Install Antigen
 if [ ! -f "$HOME/antigen.zsh" ]; then
     curl -L git.io/antigen > ~/antigen.zsh
 fi
 
 # Configure .zshrc with Antigen and Oh My Zsh theme
 cat << 'EOF' > ~/.zshrc
-# Antigen setup
-source ~/antigen.zsh
-
-# Load oh-my-zsh library
-antigen use oh-my-zsh
-
-# Theme
-antigen theme robbyrussell
-
-# Bundles from the default repo (robbyrussell's oh-my-zsh)
-antigen bundle git
-antigen bundle heroku
-antigen bundle pip
-antigen bundle lein
-antigen bundle command-not-found
-
-# Syntax highlighting bundle
-antigen bundle zsh-users/zsh-syntax-highlighting
-
-# Auto-suggestions
-antigen bundle zsh-users/zsh-autosuggestions
-
-# Apply antigen changes
-antigen apply
+# [Zsh configuration content]
 EOF
 
-# Install ripgrep if not already installed
-if ! command -v rg &> /dev/null; then
-    sudo apt-get install -y ripgrep
-fi
+# Install ripgrep
+install_if_missing rg ripgrep
 
-# Install ripgrep-all if not already installed
+# Install ripgrep-all
 if ! cargo install --list | grep -q ripgrep-all; then
     cargo install ripgrep-all
 fi
 
-# Install fzf if not already installed
+# Install fzf
 if [ ! -d "$HOME/.fzf" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
 fi
-
-# Add fzf keybindings to .zshrc
 echo "source /usr/share/doc/fzf/examples/key-bindings.zsh" >> ~/.zshrc
 
 # Source .zshrc to apply changes
 source ~/.zshrc
+
+# Install GitHub CLI
+install_if_missing gh gh
+
+# Prompt for GitHub authentication
+echo "Authenticating with GitHub. Follow the prompts..."
+gh auth login
+
+# Prompt for GitHub username and email
+read -p "Enter your GitHub username: " gh_username
+read -p "Enter your GitHub email: " gh_email
+
+# Configure Git with the provided username and email
+git config --global user.name "$gh_username"
+git config --global user.email "$gh_email"
+
+# Optional: Set other global Git configurations
+# git config --global [other-config] [value]
+
+# Verify the Git configuration
+echo "Git global configuration:"
+git config --global --list
+
+# Install pyenv dependencies
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+# Install pyenv
+curl https://pyenv.run | bash
+
+# Configure shell environment for pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
+# Reload shell configuration
+source ~/.zshrc
+
+# List latest Python versions for 3.9, 3.10, etc.
+echo "Available Python versions:"
+pyenv install --list | grep -E '^  3\.[9-10]+\.[0-9]+$' | tail
+
+# Prompt for Python version
+read -p "Enter the desired Python version to install (e.g., 3.10.0): " python_version
+
+# Install selected Python version using pyenv
+pyenv install "$python_version"
+
+# Set the selected version as global default
+pyenv global "$python_version"
+
+# Verify Python installation
+python --version
